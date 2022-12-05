a29074994b7cda6eb9ed219e50b4680ce7c43d60
Rodrigo Macario <rodrigomacario.m89@gmail.com>
2022-12-05 11:37:56 -0300

    Change & add fun

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+data class CargaHorario {
+    BASICO: (Int = 60),
+    INTERMEDIARIO: (Int = 100),
+    AVANCADO: (Int = 240);
+}
 
-class Usuario
+class Usuario (
+    Nome: String,
+    Matricula: String,
+    Nivel: String,
+    Curso: String;
+)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+    Matematica: Int,
+    Portugues: Int,
+    Geografia: Int,
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+
+data class Formacao(var nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    class <Usuario>(
+        Nome: "Paulo",
+        Matricula: "aerd",
+        Nivel: BASICO,
+        Curso: Matematica,
+    )
+   
+
 }
